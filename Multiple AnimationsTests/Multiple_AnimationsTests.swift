a6d08c136eccadc5dededc45da63626e052dca79
franciscohui <git.francisco@gmail.com>
2015-01-14 07:40:38 -0500

    Initial Commit

---
+++ Multiple AnimationsTests/Multiple_AnimationsTests.swift
@@ -0,0 +1,36 @@
+//
+//  Multiple_AnimationsTests.swift
+//  Multiple AnimationsTests
+//
+//  Created by Francisco Hui on 1/14/15.
+//  Copyright (c) 2015 Francisco Hui. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Multiple_AnimationsTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
